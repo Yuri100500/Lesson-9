62a80bbb84e18d97fabcd2ba924fe2398dc03206
Yuri100500 <flashback100500@gmail.com>
2015-03-27 16:25:41 +0200

    v.1.0.3

--- src/com/mentoring/epam/test/lesson9/testSuite/GoogleTest.java
+++ src/com/mentoring/epam/test/lesson9/testSuite/GoogleTest.java
@@ -1,4 +1,4 @@
-package com.mentoring.epam.test.lesson9.webDriverTest;
+package com.mentoring.epam.test.lesson9.testSuite;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -16,6 +16,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class GoogleTest {
 
+
     private static final String LOGIN = "myspecialtest2015";
     private static final String DOMAIN = "@gmail.com";
     private static final String PASSWORD = "fireinthehole32167";
@@ -26,96 +27,100 @@ public class GoogleTest {
 
     @BeforeClass(description = "Start Firefox. Open google")
     public void startBrowser(){
+
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://gmail.com");
     }
 
+
+
     @Test(description = "Login to mailbox")
     public void login(){
-        WebElement InputLogin = driver.findElement(By.id("Email"));
-        InputLogin.clear();
-        InputLogin.sendKeys(LOGIN);
-
-        WebElement EnteringPassword = driver.findElement(By.id("Passwd"));
-        EnteringPassword.clear();
-        EnteringPassword.sendKeys(PASSWORD);
+        driver.findElement(By.id("Email")).sendKeys(LOGIN);
+        driver.findElement(By.id("Passwd")).sendKeys(PASSWORD);
         driver.findElement(By.id("signIn")).click();
-        Assert.assertTrue(isElementPresent(By.id("gb_71")));
-        System.out.println("Ok!");
+        Assert.assertTrue(driver.findElement(By.xpath("//a[@title='Аккаунт myspecialtest2015@gmail.com']")).isDisplayed(),"Error! Unsuccessful authorization");
+
+        System.out.println("Authorization Success!!");
     }
+
 
 
     @Test(description = "Letter creation", dependsOnMethods = "login")
         public void letterCreation(){
         driver.findElement(By.xpath(".//div[contains(text(),'НАПИСАТЬ')]")).click();
-        Assert.assertTrue(isElementPresent(By.xpath(".//div[contains(@data-tooltip,'Отправить')]")));
-        System.out.println("The mail frame is open!!");
-        }
+        Assert.assertTrue(driver.findElement(By.xpath(".//div[contains(@data-tooltip,'Отправить')]")).isDisplayed(),"Error! The mail frame wasn't created!");
+    }
+
+
 
     @Test(description = "Send letter", dependsOnMethods = "letterCreation")
     public void sendNewLetter(){
         sendLetter(LOGIN + DOMAIN, TOPIC, MESSAGEBODY);
         driver.findElement(By.xpath(".//img[contains(@alt, 'Закрыть')]")).click();
-        Assert.assertTrue(isElementPresent(By.xpath(".//img[contains(@alt, 'Закрыть')]")));
-        System.out.println("Your letter was saved to drafts. Good job!");
+        Assert.assertTrue(driver.findElement(By.xpath(".//div[@gh='cm']")).isDisplayed(),"Error! The new letter wasn't close");
     }
 
+
+
     @Test(description = "Check letter in drafts",dependsOnMethods = "sendNewLetter")
-    public void checkMessage() throws InterruptedException {
+    public void checkMessage() {
         driver.findElement(By.id("gbqfq")).sendKeys("in:draft");
         driver.findElement(By.id("gbqfb")).click();
         WebDriverWait wait = new WebDriverWait(driver,10);
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@role='main']//table//tr[1]")));
         element.click();
-        Thread.sleep(1500);
-        Assert.assertTrue(isElementPresent(By.name("subjectbox")));
-        Assert.assertTrue(isElementPresent(By.xpath(".//div[contains(@role, 'textbox')]")));
+        Assert.assertTrue(driver.findElement(By.name("subjectbox")).isDisplayed(),"Error! I can't find letter's theme in drafts");
+        Assert.assertTrue(driver.findElement(By.xpath(".//div[contains(@role, 'textbox')]")).isDisplayed(), "Error! I can't find letter's body in drafts");
     }
+
+
 
     @Test(description = "Send letter", dependsOnMethods = "checkMessage")
-    public void sendLetter(){
+    public void sendLetterAgain(){
         driver.findElement(By.xpath(".//div[contains(@data-tooltip,'Отправить')]")).click();
-        Assert.assertTrue(isElementPresent(By.xpath("//font[contains(text(),'Черновик')]")));
+        Assert.assertTrue(isElementPresent(By.xpath("//font[contains(text(),'Черновик')]")),"Error, the letter is absent in drafts");
     }
 
-    @Test(description = "Check sended", dependsOnMethods = "sendLetter")
+
+
+    @Test(description = "Check sended", dependsOnMethods = "sendLetterAgain")
     public void checkSended(){
-        WebElement inputField = driver.findElement(By.id("gbqfq"));
-        inputField.clear();
-        inputField.sendKeys("in:sent");
+        WebElement element = driver.findElement(By.id("gbqfq"));
+        element.clear();
+        element.sendKeys("in:sent");
         driver.findElement(By.id("gbqfb")).click();
-        Assert.assertTrue(isElementPresent(By.xpath("//span[@email='myspecialtest2015@gmail.com'] [@name='мне']")));
+        Assert.assertTrue(isElementPresent(By.xpath("//span[@email='myspecialtest2015@gmail.com'] [@name='мне']")),"Error, the letter is not been sent");
     }
+
+
 
     @Test(description = "Logout", dependsOnMethods = "checkSended")
     public void logout(){
         driver.findElement(By.xpath("//a[@title='Аккаунт myspecialtest2015@gmail.com']")).click();
         driver.findElement(By.id("gb_71")).click();
-        Assert.assertTrue(isElementPresent(By.id("signIn")));
+        Assert.assertTrue(driver.findElement(By.id("signIn")).isDisplayed(), "Error, SignIn Element is not found");
     }
+
+
 
     @AfterClass(description = "Stop browser")
     public void tearDown(){
         driver.quit();
     }
 
-    private boolean isElementPresent(By by){
+
+
+    public  boolean isElementPresent(By by){
         return !driver.findElements(by).isEmpty();
     }
 
-    private void sendLetter(String addressee, String topic, String messageBody){
 
-        WebElement addresseeInput = driver.findElement(By.name("to"));
-        addresseeInput.clear();
-        addresseeInput.sendKeys(addressee);
 
-        WebElement topicInput = driver.findElement(By.name("subjectbox"));
-        topicInput.clear();
-        topicInput.sendKeys(topic);
-
-        WebElement messageBodyInput = driver.findElement(By.xpath(".//div[contains(@role, 'textbox')]"));
-        messageBodyInput.clear();
-        messageBodyInput.sendKeys(messageBody);
+    private  void sendLetter(String addressee, String topic, String messageBody){
+        driver.findElement(By.name("to")).sendKeys(addressee);
+        driver.findElement(By.name("subjectbox")).sendKeys(topic);
+        driver.findElement(By.xpath(".//div[contains(@role, 'textbox')]")).sendKeys(messageBody);
     }
 }
